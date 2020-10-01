b0c2f8f73bde9e459ae9dba78d7d6c0f961a31f4
karel koule <koulekarel@gmail.com>
2020-10-01 15:38:02 +0200

    reverter test index

--- test/test_index.js
+++ test/test_index.js
@@ -1,6 +1,8 @@
 require('test/helpers/prebidGlobal.js');
 require('test/mocks/adloaderStub.js');
 require('test/mocks/xhr.js');
-var testsContext = require.context('.', true, /stroeerCoreBidAdapter_spec$/);
+
+var testsContext = require.context('.', true, /_spec$/);
 testsContext.keys().forEach(testsContext);
-// window.$$PREBID_GLOBAL$$.processQueue();
+
+window.$$PREBID_GLOBAL$$.processQueue();
